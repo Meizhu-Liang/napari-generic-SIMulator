e606a6224d653a06ea7b558fc98adc56eeae4905
liangmeizhu <meizhu.liang18@ic.ac.uk>
2023-09-25 22:29:35 +0200

    test

--- src/napari_generic_simulator/_widget.py
+++ src/napari_generic_simulator/_widget.py
@@ -145,7 +145,7 @@ class PointCloud(QWidget):
             filename, _ = QFileDialog.getOpenFileName(self, 'Pick a file', filter='Files (*.pcd)')
             pc = pypcd.point_cloud_from_path(filename)
             out_arr = structured_to_unstructured(pc.pc_data)
-            print(out_arr.shape, out_arr.dtype, out_arr)
+            # print(out_arr.shape, out_arr.dtype, out_arr)
             self._viewer.add_points(-out_arr, size=0.05, name=filename[0])
         except Exception as e:
             print(e)
